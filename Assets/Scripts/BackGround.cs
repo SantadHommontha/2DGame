ce99c0b239a88ded88a78fcdb8293d7d5d100f78
SantadHommontha <santadhommontha@gmail.com>
2026-08-15 18:03:33 +0700

    up

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public float speed;
+
+    public Renderer bgRenderer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRenderer.material.mainTextureOffset += new Vector2.right * speed;
+    }
+}
